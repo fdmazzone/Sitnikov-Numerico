5deb570a1f2d492223579b81372b1bfc244c5e5f
fdmazzone <fdmazzone@gmail.com>
2018-05-07 11:32:29 -0300

    funcion que devuelve la funcion r(t)

--- Julia/Diagrama_Fases/Solve2body.jl
+++ Julia/Diagrama_Fases/Solve2body.jl
@@ -1,20 +1,23 @@
-include("SolKepler.jl")
-e=.1
-r=1;
-a=r/(1-e);
-n=a^(-1.5);
-T=2*pi/n;
-t=Array(0:.01:T);
+#include("SolKepler.jl")
+#using Interpolations
 
-M=n*t
-E=solkepler(M,e,.0001);
-dist_peri=1;
-a=dist_peri/(1-e);
-x=a*(cos.(E)-e);
-y=a*sqrt(1-e^2)*sin.(E);
-
-using Interpolations
-t=0:.01:T;
-sol_x_a = interpolate(x, BSpline(Cubic(Line())), OnGrid())
-sol_x=scale(sol_x_a,t)
-
+function Func_r(e::Float64)
+	r=1;
+	a=r/(1-e);
+	n=a^(-1.5);
+	T=2*pi/n;
+	t=Array(0:.01:T);
+	M=n*t
+	E=solkepler(M,e,.0001);
+	dist_peri=1;
+	a=dist_peri/(1-e);
+	x=a*(cos.(E)-e);
+	y=a*sqrt(1-e^2)*sin.(E);
+	t=0:.01:T;
+	sol_x_a = interpolate(x, BSpline(Cubic(Line())), OnGrid())
+	sol_x=scale(sol_x_a,t)
+	sol_y_a = interpolate(y, BSpline(Cubic(Line())), OnGrid())
+	sol_y=scale(sol_y_a,t)
+	r(t)=sqrt(sol_x[t-floor(t/T)*T]^2+sol_y[t-floor(t/T)*T]^2)
+	return r,T
+end
