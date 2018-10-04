ba58059733989f08ff6db65d11a9f39e66cac226
fdmazzone <fdmazzone@gmail.com>
2018-10-03 23:49:57 -0300

    tratando hacer andar diagrama de fases paralelo

--- Julia/Diagrama_Fases/Rombo/DiagFasesRombo-parallel.jl
+++ Julia/Diagrama_Fases/Rombo/DiagFasesRombo-parallel.jl
@@ -1,5 +1,5 @@
 @everywhere using DifferentialEquations, JLD, Interpolations, ProgressMeter
-include("Solve2body.jl")
+@everywhere include("Solve2body.jl")
 
 @everywhere λ=1.3
 @everywhere e=.1
@@ -10,8 +10,6 @@ z0_lista=linspace(5,5.1,5)
 @everywhere CantPer=10;
 nombre_arch="Borrame.jld"
 
-size_z0s=length(z0_lista)
-size_v0s=length(v0_lista)
 
 @everywhere m₂=4*(1+λ^2)^(3/2)*(8*λ^3-(1+λ^2)^(3/2))/(64*λ^3-(1+λ^2)^3)
 @everywhere m₁=4*λ^3*(1+λ^2)^(3/2)*(8-(1+λ^2)^(3/2))/(64*λ^3-(1+λ^2)^3)
@@ -36,22 +34,21 @@ end
 
 
 
-#z=SharedArray{Float64}(CantPer+1,size_z0s,size_v0s)
-#v=SharedArray{Float64}(CantPer+1,size_z0s,size_v0s)
-#Cont=1
 
-#ProBar= Progress(size_z0s*size_v0s, 1)
-
-@everywhere function DiagFases(u0)
-    prob_ode_trescuerpos = ODEProblem(Fuerza3c,u0,tspan,[m,s,r]);
-    sol = solve(prob_ode_trescuerpos,alg,dt=1e-3, progress=true,abstol = 1e-12, reltol = 1e-12);
-    return sol.(t)
+@everywhere function DiagFases(U0,p=nothing)
+    ZV=Array{Array{Float64,1},1}
+    @parallelprogress p string("Procesando...") for i in 1:length(U0) 
+        prob_ode_trescuerpos = ODEProblem(Fuerza3c,U0[i],tspan,[m,s,r]);
+        sol = solve(prob_ode_trescuerpos,alg,dt=1e-3, progress=true,abstol = 1e-12, reltol = 1e-12);
+        ZV[i]=sol.(t)
+    end
+    return ZV
 end
 
 u0=[[z,v] for z in z0_lista for v in v0_lista]
 
 
-c=pmap(DiagFases,u0)
+c=ProgressMeter.pmap(DiagFases,[u0[1:2],u0[3:end]])
 
 
 
